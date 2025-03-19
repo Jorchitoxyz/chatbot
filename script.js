46d2eab79d369122518fc193e68946fd753c07c7
jorchitoxyz <163702852+Jorchitoxyz@users.noreply.github.com>
2025-03-18 23:27:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Este archivo podría contener más lógica para animaciones interactivas
+// o para responder a eventos del usuario.
+
+console.log("¡La página está cargada y el script.js se está ejecutando!");
+
+// Puedes agregar más interacciones aquí si lo deseas.
